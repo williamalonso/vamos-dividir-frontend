838c3635da08d62905ebdec52405dc06a28264cd
William <williamalonso@outlook.com>
2024-05-24 00:32:51 -0300

    :zap: adjusting login page design

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
-import styles from '@/styles/login.module.css';
+// import styles from '@/styles/login.module.css';
 
 const Login = () => {
 
   return (
-    <div className={`w-screen h-screen bg-red-600 flex justify-center items-center`}>
-      <div className={`w-[50vw] h-full overflow-hidden ${styles.imgContainer}`}>
+    <div className={`w-screen h-screen flex justify-center items-center`}>
+      <div className={`w-[50vw] h-full overflow-hidden flex items-center justify-center`}>
         <Image 
+          className='h-auto object-contain'
           src="/login/bg-login.jpg"
           alt="Imagem de fundo"
           width={4000}
@@ -14,8 +15,20 @@ const Login = () => {
           priority 
         />
       </div>
-      <div className={`w-[50vw] h-full bg-blue-500`}>
-        meu formmeu formmeu formmeu formmeu form
+      <div className={`w-[50vw] h-full flex items-center justify-center`}>
+        <div className="text-center p-8">
+          <h2 className="text-3xl font-bold mb-4">Acesse sua conta</h2>
+          <div className="mb-4 w-full text-left">
+            <label htmlFor="email" className="block mb-1">Email</label>
+            <input type="email" id="email" className="border border-gray-300 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500" />
+          </div>
+          <div className="mb-4 w-full text-left">
+            <label htmlFor="password" className="block mb-1">Senha</label>
+            <input type="password" id="password" className="border border-gray-300 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500" />
+            <p className="text-xs text-gray-500 mt-1">Possui 8 caracteres ou mais</p>
+          </div>
+          <button className="bg-customGreen w-full text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300">Continuar</button>
+        </div>
       </div>
     </div>
   );
