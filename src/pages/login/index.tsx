94571b38a2fecf384e6ed3b8ab175b0ec871d173
William <williamalonso@outlook.com>
2024-05-26 00:13:47 -0300

    :sparkles: removing imports

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,16 +1,9 @@
 import Image from 'next/image';
-// import styles from '@/styles/login.module.css';
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
 import FilledInput from '@mui/material/FilledInput';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
-import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
