36e56181aaeefbd3a4e472446a9bae8df9267d81
William <williamalonso@outlook.com>
2024-05-26 00:36:04 -0300

    :sparkles: creating title and meta tag desc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
+import Head from 'next/head';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Vamos Dividir</title>
+        <meta name="description" content="Aplicativo web criado para organizar despesas financeiras" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
