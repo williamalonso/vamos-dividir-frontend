5b1598997e04077331af5cd4c5811d1dfdda9ba4
William <williamalonso@outlook.com>
2024-08-08 23:46:34 -0300

    :zap: updating navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import LayoutComponent from '@/components/Layout/LayoutComponent';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
@@ -9,7 +10,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <title>Vamos Dividir</title>
         <meta name="description" content="Aplicativo web criado para organizar despesas financeiras" />
       </Head>
-      <Component {...pageProps} />
+      <LayoutComponent>
+        <Component {...pageProps} />
+      </LayoutComponent>
     </>
   );
 };
