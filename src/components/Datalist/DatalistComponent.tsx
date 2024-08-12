744bd796c577d76f3266c166b475027056c36bed
William <williamalonso@outlook.com>
2024-08-12 17:51:31 -0300

    :zap: dark theme

--- src/components/Datalist/DatalistComponent.tsx
+++ src/components/Datalist/DatalistComponent.tsx
@@ -1,8 +1,12 @@
 const DatalistComponent = () => {
   return(
-    <div>
-      Item 1
-      Item 2
+    <div className="container flex flex-col shadow-lg shadow-black-500/50 p-8 bg-white dark:bg-[#3c3c3c]">
+      <div>
+        Festa aniversário Emanuelle
+      </div>
+      <div>
+        Jantar com amigos Raissa
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
@@ -13,9 +14,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-    colors: {
-      customGreen: "#24A78A", // Adicionando uma nova cor personalizada
+      colors: {
+        customGreen: "#24A78A", // Adicionando uma nova cor personalizada
+      },
     },
   },
   plugins: [],
