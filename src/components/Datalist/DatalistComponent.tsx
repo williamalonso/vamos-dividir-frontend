125c1f3b14017dac019c1ab43ba11c4c1fb237b2
William <williamalonso@outlook.com>
2024-08-12 17:57:43 -0300

    :zap: creating content area

--- src/components/Datalist/DatalistComponent.tsx
+++ src/components/Datalist/DatalistComponent.tsx
@@ -1,11 +1,13 @@
 const DatalistComponent = () => {
   return(
     <div className="container flex flex-col shadow-lg shadow-black-500/50 p-8 bg-white dark:bg-[#3c3c3c]">
-      <div>
-        Festa aniversário Emanuelle
+      <div className="flex justify-between cursor-pointer mb-2">
+        <span>Festa aniversário Emanuelle</span>
+        <span className="text-right">R$200,00</span>
       </div>
-      <div>
-        Jantar com amigos Raissa
+      <div className="flex justify-between cursor-pointer">
+        <span>Jantar com amigos Raissa</span>
+        <span className="text-right">R$75,60</span>
       </div>
     </div>
   );
