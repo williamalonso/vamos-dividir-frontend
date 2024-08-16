a9fce4e23ddc1334a5a419d1e74bff5f71b93505
William <williamalonso@outlook.com>
2024-08-16 00:15:36 -0300

    :zap: creating demand api call

--- src/pages/api/createDemand/demandService.ts
+++ src/pages/api/createDemand/demandService.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { DemandData } from '@/interfaces/demandServiceInterface';
 
 const API_URL = 'https://vamos-dividir-backend.vercel.app/api/demand/create';
-const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjUzYzdiNTQ2MWRkNWZmZjA5MDcxYjkiLCJlbWFpbCI6IndpbGxpYW0tdGVzdGVfdmVyY2VsQF9vdXRsb29rLmNvbSIsImlhdCI6MTcyMzc2OTUzNCwiZXhwIjoxNzIzODU1OTM0fQ.SMiJYK_RQLZUGClaIPfE9n2T2kJmnQ0tdzdY2H8S8kY';
+const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjUzYzdiNTQ2MWRkNWZmZjA5MDcxYjkiLCJlbWFpbCI6IndpbGxpYW0tdGVzdGVfdmVyY2VsQF9vdXRsb29rLmNvbSIsImlhdCI6MTcyMzc3NDUxNiwiZXhwIjoxNzIzODYwOTE2fQ.osoliFNFrAKr1JLJIW0CDMstBGwxkytuEcFzMSWLPoM';
 
 // Função para criar uma nova demanda
 export const createDemand = async (data: DemandData) => {
