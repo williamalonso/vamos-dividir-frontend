cfe799222da0810aa343da901def835657ccbb8f
William Alonso <williamalonso@outlook.com>
2024-08-18 21:24:27 +0000

    :zap: adjusting api create demand interface

--- src/pages/api/createDemand/demandService.ts
+++ src/pages/api/createDemand/demandService.ts
@@ -1,12 +1,12 @@
 //pages/api/createDemand/demandService.ts
 import axios from 'axios';
-import { DemandData } from '@/interfaces/demandServiceInterface';
+import { TableData } from '@/interfaces/TableData';
 
 const API_URL = 'https://vamos-dividir-backend.vercel.app/api/demand/create';
 const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjUzYzdiNTQ2MWRkNWZmZjA5MDcxYjkiLCJlbWFpbCI6IndpbGxpYW0tdGVzdGVfdmVyY2VsQF9vdXRsb29rLmNvbSIsImlhdCI6MTcyMzc3NDUxNiwiZXhwIjoxNzIzODYwOTE2fQ.osoliFNFrAKr1JLJIW0CDMstBGwxkytuEcFzMSWLPoM';
 
 // Função para criar uma nova demanda
-export const createDemand = async (data: DemandData) => {
+export const createDemand = async (data: TableData) => {
   try {
     const response = await axios.post(API_URL, data, {
       headers: {
