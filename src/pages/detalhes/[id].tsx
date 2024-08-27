5ffb1f0fb8ac2baf201478b687c38e3296b06847
William <williamalonso@outlook.com>
2024-08-26 22:58:08 -0300

    :zap: adjusting api call for especific demand id

--- src/pages/detalhes/[id].tsx
+++ src/pages/detalhes/[id].tsx
@@ -1,14 +1,40 @@
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Link from "next/link";
+import axios from "axios";
 
 const Detail = () => {
 
   const router = useRouter();
   const { id } = router.query;
   const [item, setItem] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  useEffect( () => {
+    const fetchData = async() => {
+      try {
+        setLoading(true);
+        // Buscando o token do localStorage
+        const authToken = localStorage.getItem('token');
+        if (!authToken) {
+          throw new Error('Token de autenticação não encontrado no localStorage');
+        }
+        const response = await axios.post('/api/detailDemand/detailService', {
+          token: authToken,
+          demandId: id
+        });
+        setItem(response.data);
+        console.log('Dados da demanda:', response.data);
+      } catch(e) {
+        console.error('Erro ao buscar demanda por id: ', e);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [router.query.id]);
+  
   return(
     <div className="container mx-auto">
       <Link href="/home">
